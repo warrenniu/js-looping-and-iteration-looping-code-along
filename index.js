ae456374e62e914953ea9c3209c0720785a2f04f
warrenniu <warrenniu@gmail.com>
2020-11-14 23:41:00 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let newarray = []
+    for (let i = 0; i < name.length; i++) {
+        newarray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+return newarray
+    }
+
+function countDown() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+
